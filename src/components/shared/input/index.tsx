d551de801027c37803d1338d530d3d38cb65fd46
Oleg Kolosov <dev.kolosov.oleg@yandex.ru>
2019-10-13 20:54:38 +0500

    wip

--- src/components/shared/input/index.tsx
+++ src/components/shared/input/index.tsx
@@ -1,1 +1,15 @@
-import React
+import React from 'react';
+
+interface IinputProps extends HTMLInputElement {
+  label: string;
+  placeholder: string;
+  setValue: (value: string) => void;
+}
+
+const Input = ({ label, setValue, otherProps }: inputProps) => (
+  <label>
+    {label}
+    <input onChange={( { target : { val } )=> }
+     />
+  </label>
+);
